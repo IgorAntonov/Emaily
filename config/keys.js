4a52157e863dbf81b9e445b3becaed96fd7c98ac
Igor Antonov <igorr1496@gmail.com>
2018-03-26 14:19:37 +0200

    Add a keys for production

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+const prodKeys = require('./prod');
+const devKeys = require('./dev');
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = prodKeys;
+} else {
+  module.exports = devKeys;
+}
+
