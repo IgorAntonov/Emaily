60cbc2ef6cf716bb59870e8712da1b911141e4cf
Igor Antonov <igorr1496@gmail.com>
2018-04-02 19:41:49 +0200

    add a survey creating and sending

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,12 @@ import reduxThunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
 
+
 import App from './components/App';
 import reducers from './reducers';
 
+import axios from 'axios';
+window.axios = axios;
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
